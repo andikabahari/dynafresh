53927917f39079e1fb1e6b379e4efbb713b30af4
Andika Bahari <38852468+andikabahari@users.noreply.github.com>
2024-01-13 21:31:38 +0700

    Use active tab and reload after submit

--- scripts/main.js
+++ scripts/main.js
@@ -1,49 +1,61 @@
 const form = document.getElementById('form')
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
 
-  const dynatraceProblemsPage = document.querySelector(
-    'input[name=dynatrace_problems_page]'
-  ).value
-  const refreshInterval = document.querySelector(
-    'input[name=refresh_interval]'
-  ).value
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([tab]) => {
+    if (!tab) return
 
-  chrome.permissions.request(
-    {
-      origins: [dynatraceProblemsPage],
-    },
-    (granted) => {
-      if (!granted) return
+    chrome.tabs.reload(tab.id)
 
-      chrome.tabs.query({ url: dynatraceProblemsPage }, (tabs) => {
-        chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id, allFrames: true },
-          func: (refreshInterval) => {
-            let appRoot, timeFrame, preset
-            const n = Math.ceil(Number(refreshInterval)) || 30
-            if (n < 5) n = 5
-            const seconds = 1000
-            setInterval(() => {
-              appRoot = document.querySelector('app-root')
-              if (!appRoot) return
-              timeFrame = appRoot.querySelector('a.label-box.ng-star-inserted')
-              if (timeFrame) {
-                timeFrame.click()
-                appRoot = document.querySelector('app-root')
-              }
-              preset = appRoot.querySelector(
-                'a.predefined-item.ng-star-inserted'
-              )
-              if (preset) preset.click()
-            }, n * seconds)
-          },
-          args: [refreshInterval],
+    chrome.webNavigation.onCommitted.addListener(
+      (details) => {
+        if (details.transitionType != 'reload') return
+
+        chrome.permissions.request({ origins: [tab.url] }, (granted) => {
+          if (!granted) return
+
+          const refreshInterval = document.querySelector(
+            'input[name=refresh_interval]'
+          ).value
+
+          chrome.scripting.executeScript({
+            target: { tabId: tab.id, allFrames: true },
+            func: (refreshInterval) => {
+              window.addEventListener('load', () => {
+                let appRoot, timeFrame, preset
+
+                const n = Math.ceil(Number(refreshInterval)) || 30
+                if (n < 5) n = 5
+
+                const seconds = 1000
+
+                setInterval(function () {
+                  appRoot = document.querySelector('app-root')
+                  if (!appRoot) return
+
+                  timeFrame = appRoot.querySelector(
+                    'a.label-box.ng-star-inserted'
+                  )
+                  if (timeFrame) {
+                    timeFrame.click()
+                    appRoot = document.querySelector('app-root')
+                  }
+
+                  preset = appRoot.querySelector(
+                    'a.predefined-item.ng-star-inserted'
+                  )
+                  if (preset) preset.click()
+                }, n * seconds)
+              })
+            },
+            args: [refreshInterval],
+          })
         })
-      })
-    }
-  )
+      },
+      { tadId: tab.id }
+    )
+  })
 
   window.close()
 })
